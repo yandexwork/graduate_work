5c7a87e77359fd06c3a8c1488265aa72cde7ee33
ismagkar <ismagkar4@gmil.com>
2024-01-24 13:04:19 +0400

    adding new table and alembic

--- billing-api/src/models/tariff.py
+++ billing-api/src/models/tariff.py
@@ -1,7 +1,8 @@
 from uuid import uuid4
 
-from sqlalchemy import Column, String, Text, Integer, Boolean, DateTime, Numeric
+from sqlalchemy import Column, String, Text, Integer, Boolean, DateTime, Numeric, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import mapped_column
 from sqlalchemy.sql import func
 
 from db.postgres import Base
@@ -16,5 +17,16 @@ class Tariff(Base):
     price = Column(Numeric(6, 2), nullable=False)
     duration = Column(Integer, default=1)
     is_active = Column(Boolean, default=True)
+    created = Column(DateTime(timezone=True), server_default=func.now())
+    modified = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class Payment(Base):
+    __tablename__ = 'payment'
+    __table_args__ = {'schema': 'content'}
+
+    id = mapped_column(Integer, primary_key=True, unique=True, nullable=False)
+    user_id = Column(UUID(as_uuid=True), nullable=False)
+    tariff_id = Column(ForeignKey("tariff.id", ondelete="SET NULL"), nullable=False)
     created = Column(DateTime(timezone=True), server_default=func.now())
     modified = Column(DateTime(timezone=True), onupdate=func.now())
