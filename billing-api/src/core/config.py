6913b5171619eb519c456318698f41e623692dbc
Danila Kalganov <dkalganov@team.amocrm.com>
2024-01-25 22:18:22 +0300

    feat: добавлена авторизация

--- billing-api/src/core/config.py
+++ billing-api/src/core/config.py
@@ -1,4 +1,18 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from async_fastapi_jwt_auth import AuthJWT
+
+
+class AuthJWTSettings(BaseSettings):
+    authjwt_access_cookie_key: str = 'access_token'
+    authjwt_refresh_cookie_key: str = 'refresh_token'
+    authjwt_cookie_csrf_protect: bool = False
+    authjwt_token_location: set = {"cookies", "headers"}
+    authjwt_secret_key: str = 'some_key'
+
+
+@AuthJWT.load_config
+def get_auth_settings() -> AuthJWTSettings:
+    return AuthJWTSettings()
 
 
 class PostgresSettings(BaseSettings):
@@ -17,6 +31,7 @@ class Setting(BaseSettings):
     yookassa_token: str
     yookassa_shopid: str
     webhook_api_url: str
+    payment_redirect_url: str
 
 
 settings = Setting()
