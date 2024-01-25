5e78b00a66b13f5588bb8b0cfecc7f044e12fcdf
Danila Kalganov <dkalganov@team.amocrm.com>
2024-01-25 22:18:35 +0300

    feat: добавлены новые ошибки

---
+++ billing-api/src/core/exceptions.py
@@ -0,0 +1,28 @@
+from typing import Any
+from http import HTTPStatus
+
+
+class BaseError(Exception):
+    ...
+
+
+class BaseErrorWithContent(BaseError):
+    status_code: int
+    content: dict[str, Any]
+
+
+class AlreadySubscribedError(BaseErrorWithContent):
+    status_code = HTTPStatus.CONFLICT
+    content = {'message': 'User already subscribed'}
+
+
+class TariffNotFoundError(BaseErrorWithContent):
+    status_code = HTTPStatus.NOT_FOUND
+    content = {'message': 'Tariff is not found'}
+
+
+class UserUnauthorizedError(BaseErrorWithContent):
+    status_code = HTTPStatus.UNAUTHORIZED
+    content = {'message': 'User is unauthorized'}
+
+
