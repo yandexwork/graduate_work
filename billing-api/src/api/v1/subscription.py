9de2937278a823006d3efa3bf6a67fe0c3d0acdf
ismagkar <ismagkar4@gmil.com>
2024-01-28 19:45:11 +0400

    добавлены ручки для  отписки и для возврата денег

--- billing-api/src/api/v1/subscription.py
+++ billing-api/src/api/v1/subscription.py
@@ -1,11 +1,11 @@
 from http import HTTPStatus
+from uuid import UUID
 
 from fastapi import APIRouter, Depends
 
 from services.payment.yookassa_service import YookassaService, get_yookassa_service
-from services.jwt_service import get_user_data_from_jwt
+from services.jwt_service import get_user_id_from_jwt
 from schemas.payment import CreatePaymentSchema, CreatedPaymentSchema
-from schemas.user import UserSchema
 
 
 router = APIRouter()
@@ -18,7 +18,34 @@ router = APIRouter()
              status_code=HTTPStatus.CREATED)
 async def subscribe(
         payment_data: CreatePaymentSchema,
-        user_data: UserSchema = Depends(get_user_data_from_jwt),
+        user_id: UUID = Depends(get_user_id_from_jwt),
         payment_service: YookassaService = Depends(get_yookassa_service)
 ) -> CreatedPaymentSchema:
-    return await payment_service.create_payment(user_data.user_id, payment_data.tariff_id)
+    return await payment_service.create_payment(user_id, payment_data.tariff_id)
+
+
+@router.post('/cancellation',
+             summary="Отписка с возвратом",
+             status_code=HTTPStatus.OK)
+async def unsubscribe(
+        user_id: UUID,
+        admin_id: UUID = Depends(get_user_id_from_jwt),
+        payment_service: YookassaService = Depends(get_yookassa_service),
+        return_found: bool = False,
+) -> int:
+    # todo добавить в токен роль чтобы разграничивать поведение
+    # if not admin_id.get('is_admin'):
+    #     return HTTPStatus.FORBIDDEN
+    # else:
+    #     return await payment_service.unsubscribe(user_id, return_found)
+    return await payment_service.unsubscribe(user_id, return_found)
+
+
+@router.post('/unsubscribe',
+             summary="Отписка",
+             status_code=HTTPStatus.OK)
+async def unsubscribe(
+        user_id: UUID = Depends(get_user_id_from_jwt),
+        payment_service: YookassaService = Depends(get_yookassa_service),
+) -> int:
+    return await payment_service.unsubscribe(user_id, False)
